2fd665c504040f2e0450ad9730070102b9e9b067
ayushhh321 <ayush56039@gmail.com>
2023-06-04 23:59:02 +0530

    phase 2

--- script.py
+++ script.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 # Load Excel file into DataFrame
 df = pd.read_excel('pincodes_Ids_latest_excel.xlsx')
+
+# Convert DataFrame to JSON string
+json_data = df.to_json(orient='records')
